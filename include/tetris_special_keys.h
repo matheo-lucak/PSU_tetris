8a4cb3d22632a2b8c9796ed0d9090e0014738142
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-04-04 04:13:32 +0200

    [Special Keys Struct Update] -> removed useless .itteration struct part

--- include/tetris_special_keys.h
+++ include/tetris_special_keys.h
@@ -13,18 +13,17 @@
 #include <ncurses.h>
 
 struct special_key {
-    size_t itteration;
     int character;
     char *to_print;
 };
 
 static struct special_key const special_keys[6] = {
-    {.itteration = 0, .character = ' ', .to_print = "(space)\n"},
-    {.itteration = 1, .character = KEY_LEFT, .to_print = "^EOD\n"},
-    {.itteration = 2, .character = KEY_RIGHT, .to_print = "^EOC\n"},
-    {.itteration = 3, .character = KEY_UP, .to_print = "^EOA\n"},
-    {.itteration = 4, .character = KEY_DOWN, .to_print = "^EOB\n"},
-    {.itteration = 5, .character = -1, .to_print = NULL},
+    {.character = ' ', .to_print = "(space)\n"},
+    {.character = KEY_LEFT, .to_print = "^EOD\n"},
+    {.character = KEY_RIGHT, .to_print = "^EOC\n"},
+    {.character = KEY_UP, .to_print = "^EOA\n"},
+    {.character = KEY_DOWN, .to_print = "^EOB\n"},
+    {.character = -1, .to_print = NULL},
 };
 
 #endif /* TETRIS_SPECIAL_KEYS_H_ */
